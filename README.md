5709078c1e5f040eee36f4de7d98e592ce0571d1
Jbarbss <jbarbanel@gmail.com>
2021-04-09 11:55:29 -0400

    added license info to license.js

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# ;rcje;jr
+# Update Portfolio
 
+  
+  ### Table Of Contents
+
+  1. [Description](#Description)
+  2. [Installation](#Installation)
+  4. [Usage](#Usage)
+  5. [Contributions](#Contributions)
+  6. [Contact](#Contact)
+  ## Description 
+
+  I have updated my portfolio with the new skills I have learned
+
+  To showcase my skills
+
+  Showing off my resume to potential job offers
+
+  how to make a mobile first resposive site
+
+  
+  
+  
+ ## Installation
+
+ launch and build
+
+ [Github Pages Live Site] https://jbarbss.github.io/Updated-Portfolio/
+
+ [Github Repo Site] https://github.com/Jbarbss/Updated-Portfolio
+ ## Usage
+ undefined
+
+ ![ScreenShot](undefined "ScreenShot")
+ 
+ ## License
+
+ undefined
+
+ undefined
+
+ undefined;
+ 
+ 
+ ## Contributing
+
+ Ben Durham (https://github.com/bdurham227)
+
+ Gabe Thomas (https://github.com/samohtebag)
+
+ 
+
+ ## Contact
+
+ 
+ My github: https://github.com/Jbarbss
+
+ my Email: Jbarbanel@gmail.com
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,10 +11,52 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers, license) {
 
+  return `# ${answers.title}\n
+  
+  ### Table Of Contents\n
+  1. [Description](#Description)
+  2. [Installation](#Installation)
+  4. [Application Screenshot](#Application Screenshot)
+  5. [Contributions](#Contributions)
+  6. [Contact](#Contact)
+  ## Description \n
+  ${answers.description}\n
+  ${answers.reason}\n
+  ${answers.problem}\n
+  ${answers.learn}\n
+  
+  
+  
+ ## Installation\n
+ ${answers.install}\n
+ [Github Pages Live Site] ${answers.githubPages}\n
+ [Github Repo Site] ${answers.githubRepo}
+ ## Application Screenshot
+ ![ScreenShot](${answers.screenshot} "ScreenShot")
+ 
+ ## License\n
+ ${renderLicenseBadge(answers.license)}\n
+ ${renderLicenseLink(answers.license)}\n
+ ${renderLicenseSection(answers.license)};
+ 
+ 
+ ## Contributing\n
+ ${answers.contributions}\n
+ ${answers.contributors}\n
+ 
 
+ ## Contact\n
+ 
+ My github: ${answers.githubUrl}\n
+ my Email: ${answers.email}
+  `;
 }
+
+
+
+
+
 
 module.exports = generateMarkdown;
